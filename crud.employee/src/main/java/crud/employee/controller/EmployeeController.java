e6ddf7b38bbff64d9b6a720b2d62843d71796ea3
deep473 <deepztanshu@gmail.com>
2025-06-26 16:45:11 +0530

    create

--- crud.employee/src/main/java/crud/employee/controller/EmployeeController.java
+++ crud.employee/src/main/java/crud/employee/controller/EmployeeController.java
@@ -1,20 +1,27 @@
 package crud.employee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import crud.employee.entity.Employee;
 import crud.employee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-@RestController
+
+@Controller
 public class EmployeeController {
 	@Autowired
 	EmployeeService service;
 	
 	@PostMapping("/create")
-	public void createNewEmployee(@RequestBody Employee emp) {
+	public void createNewEmployee(@ModelAttribute Employee emp) {
 		service.createNewEmployee(emp);
 	}
+	
+	@GetMapping("/createPage")
+	public String createPage() {
+		return "create";	//create.html
+	}
+	
 }
